cf9ca4884c9c56f4c2d274d460790a78bc30ba77
zhenhao-ma <bob0103779@gmail.com>
2024-07-26 17:53:00 +0800

    update

--- src/batch_fn.rs
+++ src/batch_fn.rs
@@ -2,9 +2,10 @@ use async_trait::async_trait;
 use std::collections::HashMap;
 
 #[async_trait]
-pub trait BatchFn<K, V> {
-    async fn load(&mut self, keys: &[K]) -> HashMap<K, V>
+pub trait BatchFn<K, V, C> {
+    async fn load(&mut self, keys: &[K], context: &C) -> HashMap<K, V>
     where
         K: 'async_trait,
-        V: 'async_trait;
+        V: 'async_trait,
+        C: 'async_trait;
 }
